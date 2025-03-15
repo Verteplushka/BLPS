6e7ad8763c07127876dcf4319aab6e2e9f6fe2b3
Verteplushka <kolbacin_vlad@mail.ru>
2025-03-15 10:24:57 +0300

    Initial commit

---
+++ src/test/java/com/example/BLPS/BlpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BLPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
